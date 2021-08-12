b2019352b3a2c0e938798ad528b706676c535711
Delvin So <22638338+delvinso@users.noreply.github.com>
2021-08-12 17:23:55 -0400

    updates from meeting - change BRCA to dropdown w/ unknown, account for unknown on backend and front-end related changes

--- flask/project/forms.py
+++ flask/project/forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import  SubmitField, IntegerField, FloatField, BooleanField, RadioField, SelectField
+from wtforms import (
+    SubmitField,
+    IntegerField,
+    FloatField,
+    BooleanField,
+    RadioField,
+    SelectField,
+)
 from wtforms.validators import DataRequired
+
 
 class CalculatorForm(FlaskForm):
     """
@@ -11,43 +19,71 @@ class CalculatorForm(FlaskForm):
     """
 
     # mandatory fields
-    age = IntegerField('Age (years)', validators = [DataRequired(message = "This should be in years, eg. 18, an integer."), ])
-    ecog = IntegerField('ECOG', validators = [DataRequired()])
-    alb = FloatField('Albumin', validators = [DataRequired()])
-    ca_125 = IntegerField('CA-125', validators = [DataRequired()])
-    brca = BooleanField('BRCA', default = 0)
+    age = IntegerField(
+        "Age (years)",
+        validators=[
+            DataRequired(message="This should be in years, eg. 18, an integer."),
+        ],
+    )
+    ecog = IntegerField("ECOG", validators=[DataRequired()])
+    alb = FloatField("Albumin", validators=[DataRequired()])
+    ca_125 = IntegerField("CA-125", validators=[DataRequired()])
+    brca = SelectField(
+        "BRCA",
+        choices=[(999999, "U"), (0, "N"), (1, "Y")],
+        # validators=[DataRequired()],
+        default=999999,
+        coerce=int,
+    )
 
     # surgical resectability (n = 8)
 
-    splenic_hilum = BooleanField('Splenic Hilum or Splenic Ligaments Lesion', default = 0)
-    gastrohepatic_lig = BooleanField("Gastrohepatic Ligament or Porta Hepatis Lesion",  default = 0)
-    retroperitoneal_suprarenal_ln = BooleanField("Retroperitoneal Lymph Nodes above the Renal Hilum",  default = 0)
-    sb_adhesions = BooleanField("Diffuse Small Bowel Adhesions or Thickening", default = 0)
-    ascites = RadioField("Moderate-Severe Abdominal Ascites or CA-125 >600", 
-                        choices = [(1, 'Low'), (2, 'Moderate'), (3, 'Severe')], 
-                        validators =  [DataRequired()], default = 1)
+    splenic_hilum = BooleanField("Splenic Hilum or Splenic Ligaments Lesion", default=0)
+    gastrohepatic_lig = BooleanField(
+        "Gastrohepatic Ligament or Porta Hepatis Lesion", default=0
+    )
+    retroperitoneal_suprarenal_ln = BooleanField(
+        "Retroperitoneal Lymph Nodes above the Renal Hilum", default=0
+    )
+    sb_adhesions = BooleanField(
+        "Diffuse Small Bowel Adhesions or Thickening", default=0
+    )
+    ascites = RadioField(
+        "Moderate-Severe Abdominal Ascites or CA-125 >600",
+        choices=[(1, "Low"), (2, "Moderate"), (3, "Severe")],
+        # validators=[DataRequired()],
+        default=1,
+        coerce=int,
+    )
     # CHECK WITH SABRINA - looks binary to me
-    gb_fossa = BooleanField("Gallbladder Fossa or Liver Intersegmental Fissure Lesion", default = 0)
-    lesser_sac = BooleanField("Lesser Sac Lesion >1cm", default = 0)
-    sma_root = BooleanField("Root of Superior Mesenteric Artery Lesion", default = 0)
+    gb_fossa = BooleanField(
+        "Gallbladder Fossa or Liver Intersegmental Fissure Lesion", default=0
+    )
+    lesser_sac = BooleanField("Lesser Sac Lesion >1cm", default=0)
+    sma_root = BooleanField("Root of Superior Mesenteric Artery Lesion", default=0)
 
     # surgical complexity (n = 11)
 
-    pre_tahbso_omx = BooleanField("TAH-BSO + Omentectomy", default = 0)
-    pre_plnd = BooleanField("Pelvic Lymphadenectomy ", default = 0)
-    pre_palnd = BooleanField("Para-Aortic Lymphadenectomy", default = 0)
-    pre_pelvicperit = BooleanField("Pelvic Peritoneum Stripping", default = 0)
-    pre_abdoperit = BooleanField("Abdominal Peritoneum Stripping", default = 0)
-    pre_bowelresect = RadioField("Bowel Resection",
-                        choices = [(0, 'Small Bowel'), (1,  'Large Bowel'), (2, 'Both')], 
-                        validators =  [DataRequired()], 
-                        default = 0) # should be ordinal SB, LB, Both
-    pre_diaphr = BooleanField("Diaphragm Stripping or Resection", default = 0)
-    pre_splen = BooleanField("Splenectomy", default = 0)
-    pre_liver = BooleanField("Liver Resection", default = 0)
-    pre_lar = BooleanField("Rectosigmoidectomy with Reanstomosis", default = 0)
-    pre_vats = BooleanField("VATS/Intrathoracic Resection ", default = 0)
+    pre_tahbso_omx = BooleanField("TAH-BSO + Omentectomy", default=0)
+    pre_plnd = BooleanField("Pelvic Lymphadenectomy ", default=0)
+    pre_palnd = BooleanField("Para-Aortic Lymphadenectomy", default=0)
+    pre_pelvicperit = BooleanField("Pelvic Peritoneum Stripping", default=0)
+    pre_abdoperit = BooleanField("Abdominal Peritoneum Stripping", default=0)
+    pre_bowelresect = RadioField(
+        "Bowel Resection",
+        choices=[(0, "Small Bowel"), (1, "Large Bowel"), (2, "Both")],
+        # validators=[DataRequired()],
+        default=0,
+        coerce=int,
+    )  # should be ordinal SB, LB, Both
+    pre_diaphr = BooleanField("Diaphragm Stripping or Resection", default=0)
+    pre_splen = BooleanField("Splenectomy", default=0)
+    pre_liver = BooleanField("Liver Resection", default=0)
+    pre_lar = BooleanField("Rectosigmoidectomy with Reanstomosis", default=0)
+    pre_vats = BooleanField("VATS/Intrathoracic Resection ", default=0)
 
-    st_4unresec = BooleanField("Stage 4 Unresection?? ", default = 0)
-    
-    submit = SubmitField('Submit')
+    st_4unresec = BooleanField(
+        "Stage 4 Unresection (Used only in NGR prediction) ", default=0
+    )
+
+    submit = SubmitField("Submit")
--- flask/project/routes.py
+++ flask/project/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, jsonify, render_template, request, current_app as app, abort
+from flask import (
+    Blueprint,
+    jsonify,
+    render_template,
+    request,
+    current_app as app,
+    abort,
+)
 from .forms import CalculatorForm
 from project import ngr, opt
 
@@ -9,13 +16,20 @@ routes = Blueprint("routes", __name__)
 def calculator():
     """
     Our ML app
-    
+
     Luckily wtforms provides input validation
     """
     form = CalculatorForm()
     if form.validate_on_submit():
 
         dat = form.data.copy()
+        # 999999 is the associated value with 'None' which needs to be converted.
+        # This is done as SelectField coerces the choice to an int + None is not a valid input, see : https://stackoverflow.com/questions/46335763/make-a-selectfield-select-nothing-by-default
+        # assume valid if we're here, except for Nones which need to be converted
+        for k in dat:
+            if dat[k] == 999999:
+                dat[k] = None
+        # app.logger.debug(predict(form.data))
         app.logger.debug(dat)
         predictions = predict(dat)
         # return(jsonify(predict(form.data)))
@@ -23,19 +37,17 @@ def calculator():
             "calculator.jinja2",
             form=form,
             template="form-template",
-            predictions=predictions
+            predictions=predictions,
         )
     app.logger.debug("Form Errors: {}".format(form.errors))
-    return render_template(
-        "calculator.jinja2",
-        form=form,
-        template="form-template"
-    )
+    return render_template("calculator.jinja2", form=form, template="form-template")
 
-@routes.route("/api/predict", methods = ["POST"])
+
+@routes.route("/api/predict", methods=["POST"])
 def predict2():
     data = request.json
     return jsonify(predict(data))
+
 
 def predict(data):
 
@@ -46,22 +58,27 @@ def predict(data):
     model_vars = list(ngr.getInputMetadata().keys())
     # missing_vars = [k for k in model_vars if k not in data]
 
-    # if len(missing_vars) > 0:   
+    # if len(missing_vars) > 0:
     #     app.logger.error('Not all form variables were found in the model metadata...please check')
     #     abort(500)
 
-    relevant_d = {k:data[k] for k in list(ngr.getInputMetadata().keys()) if k in data}
+    relevant_d = {k: data[k] for k in list(ngr.getInputMetadata().keys()) if k in data}
 
     if len(relevant_d) != len(model_vars):
-        app.logger.error('Not all form variables were found in the model metadata...please check')
+        app.logger.error(
+            "Not all form variables were found in the model metadata...please check"
+        )
         abort(500)
 
     app.logger.debug(relevant_d)
 
     # ngr model prediction occurs first as it makes use of st_4unresec. it is subsequently removed in the opt model.
-    ngr_score, opt_score = ngr.score(indata = relevant_d, outdata = ngr.getOutputMetadata()), \
-                           opt.score(indata = relevant_d, outdata = opt.getOutputMetadata())
+    ngr_score, opt_score = (
+        ngr.score(indata=relevant_d, outdata=ngr.getOutputMetadata()),
+        opt.score(indata=relevant_d, outdata=opt.getOutputMetadata()),
+    )
 
-    return {'opt_score': opt_score['Prob(pds_opt==1)_1'], 'ngr_score' : ngr_score['Prob(pds_ngr==1)_1']}
-
-
+    return {
+        "Optimal Cytoreduction": opt_score["Prob(pds_opt==1)_1"],
+        "No Growth Residual   ": ngr_score["Prob(pds_ngr==1)_1"],
+    }
